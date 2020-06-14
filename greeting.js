1bfb2b032d0fd37e77fe888e7989c90183f97315
MijeeKim <64686316+MijeeKim@users.noreply.github.com>
2020-06-15 02:22:34 +0900

    delete code

--- greeting.js
+++ greeting.js
@@ -26,7 +26,6 @@ function askForName(){
 
 function paintGreeting(text){
     const number = Math.floor(Math.random()*RANDOM_NUMBER);
-    // console.log(ment[number]);
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `${ment[number]} ${text}♥`;
